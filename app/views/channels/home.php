aaed735f8ca4787ebd91a827ec160bac32a0a5d2
youzyie <youzyie@gmail.com>
2026-05-19 14:33:13 +0700

    feat: fix-home-error

--- app/views/channels/home.php
+++ app/views/channels/home.php
@@ -34,21 +34,43 @@
                         <h3>Your Progress</h3>
                         <a href="#" class="see-detail">See Detail</a>
                     </div>
-                    <div class="progress-cards-flex">
-                        <?php 
-                        $items = [['MTK dasar', 80], ['English Discovery', 40], ['Preparing For TOEFL', 50]];
-                        for ($i = 0; $i < 2; $i++): ?>
-                        <div class="mini-card">
-                            <?php foreach($items as $item): ?>
-                            <div class="bar-row">
-                                <span class="subject"><?= $item[0] ?></span>
-                                <div class="bar-bg"><div class="bar-fill" style="width: <?= $item[1] ?>%;"></div></div>
-                                <span class="percentage"><?= $item[1] ?>%</span>
-                            </div>
-                            <?php endforeach; ?>
-                        </div>
-                        <?php endfor; ?>
-                    </div>
+                 <div class="progress-cards-flex">
+    <?php 
+    // Data untuk KOTAK SEBELAH KIRI
+    $items_kiri = [
+        ['MTK dasar', 90], 
+        ['English Discovery', 40], 
+        ['Preparing For TOEFL', 50]
+    ];
+
+    // Data untuk KOTAK SEBELAH KANAN
+    $items_kanan = [
+        ['MTK', 80], 
+        ['English Discovery', 40], 
+        ['Preparing For TOEFL', 50]
+    ];
+    ?>
+
+    <div class="mini-card">
+        <?php foreach($items_kiri as $item): ?>
+        <div class="bar-row">
+            <span class="subject"><?= $item[0] ?></span>
+            <div class="bar-bg"><div class="bar-fill" style="width: <?= $item[1] ?>%;"></div></div>
+            <span class="percentage"><?= $item[1] ?>%</span>
+        </div>
+        <?php endforeach; ?>
+    </div>
+
+    <div class="mini-card">
+        <?php foreach($items_kanan as $item): ?>
+        <div class="bar-row">
+            <span class="subject"><?= $item[0] ?></span>
+            <div class="bar-bg"><div class="bar-fill" style="width: <?= $item[1] ?>%;"></div></div>
+            <span class="percentage"><?= $item[1] ?>%</span>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</div>
                 </div>
             </section>
 
@@ -57,10 +79,10 @@
             <section class="content-section">
                 <h2 class="section-title">Today Motivations</h2>
                 <div class="grid-container-motive">
-                    <div class="card-img"><img src="motive1.jpg" alt="Motive 1"></div>
-                    <div class="card-img"><img src="motive2.jpg" alt="Motive 2"></div>
-                    <div class="card-img"><img src="motive3.jpg" alt="Motive 3"></div>
-                    <div class="card-img"><img src="motive4.jpg" alt="Motive 4"></div>
+                    <div class="card-img"><img src="/assets/motivation/motive1.jpg" alt="motive 1"></div>
+                    <div class="card-img"><img src="app/public/assets/motivation/motive2.jpg" alt="Motive 2"></div>
+                    <div class="card-img"><img src="app/public/assets/motivation/motive3.jpg" alt="Motive 3"></div>
+                    <div class="card-img"><img src="app/public/assets/motivation/motive4.jpg" alt="Motive 4"></div>
                 </div>
             </section>
 
